5a54e0317550ef019883786aab5b2fbf79804d38
Tetiana Skvarnyk <t.skvarnikgmail.com>
2024-02-29 22:12:36 +0100

    Added Login Steps,Hooks, BaseSteps

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,9 +1,27 @@
 package steps;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import steps.BaseSteps;
 
-public class LoginSteps extends BaseSteps{
+public class LoginSteps extends BaseSteps {
+    @When("I fill username")
+    public void iFillUsername() {
+    }
 
-    @When()
 
+    @And("I fillpassword")
+    public void iFillpassword() {
+    }
+
+    @And("I click to login button")
+    public void iClickToLoginButton() {
+
+
+    }
+
+    @Then("I am on the Products page")
+    public void iAmOnTheProductsPage() {
+    }
 }

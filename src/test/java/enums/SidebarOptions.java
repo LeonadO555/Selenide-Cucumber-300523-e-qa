589de495e886fcd13ed28938b093f46af3c006d6
Pislaru <pislaru.daniel1@gmail.com>
2024-03-06 00:10:11 +0100

    New Test

---
+++ src/test/java/enums/SidebarOptions.java
@@ -0,0 +1,17 @@
+package enums;
+
+import lombok.Getter;
+
+@Getter
+public enum SidebarOptions {
+        INVENTORY("inventory"),
+        ABOUT("about"),
+        LOGOUT("logout"),
+        RESET("reset");
+        public final String value;
+
+
+        SidebarOptions(String value) {
+            this.value = value;
+        }
+    }
---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,64 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import enums.SidebarOptions;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.value;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class BasePage {
+    public Header header;
+    private final Sidebar sidebar;
+
+    public BasePage(){
+        this.header = new Header();
+        this.sidebar = new Sidebar();
+    }
+
+    public void setInput(SelenideElement input, String value){
+        input.shouldBe(visible,enabled,editable)
+                .click();
+        input.clear();
+        input.setValue(value)
+                .shouldBe(value(value));
+    }
+
+    public void click(SelenideElement element){
+        element.shouldBe(visible,enabled)
+                .click();
+    }
+
+    public void openCartPage(){
+        click(header.cartButton);
+        sidebar.sidebar.shouldBe(visible);
+    }
+
+    public void openSidebar(){
+        click(header.burgerButton);
+    }
+
+    public void goToSidebarOption(SidebarOptions tab){
+        openSidebar();
+        click($("[//*[@id='"+ tab.value + "_sidebar_link']"));
+    }
+
+    public void closeSidebar(){
+        click(sidebar.closeButton);
+        sidebar.sidebar.shouldBe(hidden);
+    }
+}
+
+
+class Header{
+    SelenideElement cartButton = $x("//*[@class='shopping_cart_link']");
+    public SelenideElement burgerButton = $x("//*[@id='react-burger-menu-btn']");
+}
+
+
+class Sidebar{
+    SelenideElement sidebar = $x("[@class='bm-item-list']");
+    SelenideElement closeButton = $x("//*[@id='react-burger-cross-btn']");
+
+}
